62d7ec961a053df62f2be25581f146ac3813b60e
uwall <iowamoira24@gmail.com>
2020-07-14 14:42:34 +0900

    xml 파일 이름 변경
    activity_permission

--- app/src/main/java/com/example/ts_capture_pliot/PermissionActivity.java
+++ app/src/main/java/com/example/ts_capture_pliot/PermissionActivity.java
@@ -16,7 +16,7 @@ public class PermissionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_permission);
 
         requestPermission();
     }
